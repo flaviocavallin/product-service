a6e88cb635aec12979f329dc5ddf55d77813ae8f
Flavio <flaviocavallin@gmail.com>
2020-06-07 21:51:22 +0100

    Change logic

--- src/main/java/com/example/productservice/controller/ProductController.java
+++ src/main/java/com/example/productservice/controller/ProductController.java
@@ -15,22 +15,11 @@ public class ProductController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ProductController.class);
 
-    private static InetAddress ip;
-
-    static {
-        try {
-            ip = InetAddress.getLocalHost();
-        } catch (Exception e) {
-            LOGGER.error("There was an error trying to execute the operation", e);
-            throw new RuntimeException("There was an error", e);
-        }
-    }
-
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public String get() {
-        String host = "1 - HostName:" + ip.getHostName() + " - CanonicalHostName:" + ip.getCanonicalHostName() + " - HostAddress:" + ip.getHostAddress();
-        LOGGER.info(host);
-        return host;
+        String result = "Hello World!!!";
+        LOGGER.info(result);
+        return result;
     }
 
 }
